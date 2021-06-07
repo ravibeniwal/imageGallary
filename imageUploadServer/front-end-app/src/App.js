1a07c9e3f9772ea9216f41e69d15d056567e0f65
ravibeniwal <ravi.beniwal@simbaquartz.com>
2021-06-07 13:20:03 +0530

    modified code and removed spaces and console

--- imageUploadServer/front-end-app/src/App.js
+++ imageUploadServer/front-end-app/src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import "antd/dist/antd.css";
 import ImageGallary from "./ImagesGallary";
 import ImageTableView from "./ImageTableView";
-import { Button } from "antd";
-import { useState } from "react";
+import {Button} from "antd";
+import {useState} from "react";
 
 function App() {
   const [mapView, setMapView] = useState(false);
@@ -11,9 +11,9 @@ function App() {
     <div className="App">
       {mapView ? <ImageTableView /> : <ImageGallary />}
       <Button
-      style={{margin:"20px 10px"}}
-      size="large"
-      type="primary"
+        style={{margin: "20px 10px"}}
+        size="large"
+        type="primary"
         onClick={() => {
           setMapView(!mapView);
         }}
--- imageUploadServer/server/app.js
+++ imageUploadServer/server/app.js
@@ -55,7 +55,6 @@ mongoose.connect(
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
